a47ba726d74c16ef7720983cc4e97ccab3ea4f70
sagarpatil190 <sagarpatil190@gmail.com>
2024-12-23 23:46:52 +0530

    passwrod genertor compelete application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,53 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'password_generator';
+  length = 0
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+  password = '';
+  
+  onChangeLength(e:any){
+    let value = e.target.value
+
+    if(!isNaN(value) && value > 0 ){
+      this.length = value;
+    }else{
+      this.length =0;
+    }
+
+  }
+  onChangeUseLetters() {
+    this.useLetters = !this.useLetters;
+  }
+  onChangeUseNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+  onChangeUseSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onButtonClick(){
+    let letters = "abcdefghijklmnopqrstuvwxyz";
+    let numbers = "1234567890";
+    let symbols = "!@#$%^&*()";
+
+    let validChars = '';
+    if(this.useLetters){
+      validChars += letters;
+    }
+    if(this.useNumbers){
+      validChars += numbers;
+    }
+    if(this.useSymbols){
+      validChars += symbols;
+    }
+    let geneRatedPassword = '';
+    for(let i =0; i< this.length; i++){
+      const randomNum = Math.floor(Math.random() * validChars.length);
+      geneRatedPassword += validChars[randomNum];
+    }
+    this.password = geneRatedPassword;
+  }
+
 }
